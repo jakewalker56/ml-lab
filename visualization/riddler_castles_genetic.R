a7c4839991bd6d95941216589482672093bed446
Jacob Walker <jakewalker@chicagobooth.edu>
2017-02-11 02:34:02 -0800

    adding genetic algorithm

--- visualization/riddler_castles_genetic.R
+++ visualization/riddler_castles_genetic.R
@@ -65,15 +65,16 @@ for(i in 1:nrow(s)) {
 s <- s[-discard,]
 rownames(s) <- seq(from=1, to=nrow(s), 1)
 
+#Score everyone against everyone else, just for fun
 #578.567 seconds = ~10 min
-#system.time(results <- fast_evaluate(s))
-
+system.time(results <- fast_evaluate(s))
 sapply(seq(from=1, to=nrow(s), by=1), function(i){
   return(paste(list(s[i,]), ": ", results[i]))
 })
-
 print(paste("winning strategy: ", list(s[which.is.max(results),])))
 
+#look at the top 100 winning strategies
+#winningest strategy is a good baseline for everything else (1137 wins)
 n=100
 p = n
 print(paste("top ", n, " strategies:", sep=""))
@@ -82,56 +83,32 @@ for(i in tail(order(results), n)) {
   p <- p - 1
 }
 
-mystrategy = c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-mystrategy = c(6, 2, 2, 12, 16, 3, 2, 31, 4, 22)
-#4, 5, 8, 10
-sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
-
-mystrategy = c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-mystrategy = c(0, 3, 2, 12, 3, 22, 2, 31, 3, 22)
-#4, 6, 8, 10
-sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
-
-mystrategy = c(1, 1, 11, 12, 1, 19, 24, 29, 1, 1)
-mystrategy = c(0, 1, 7, 8, 2, 22, 22, 31, 4, 3)
-#3, 4, 6, 7, 8
-sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
-
-mystrategy = c(3, 5, 8, 10, 13, 1, 26, 30, 2, 2)
-mystrategy = c(0, 6, 8, 9, 12, 1, 27, 31, 2, 4)
-#2, 3, 4, 5, 7
-sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
-
-mystrategy = c(0, 5, 7, 9, 16, 22, 2, 2, 33, 4)
-#2, 3, 4, 5, 6, 9
-sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
-
-
+#utility function for genetic algorithm
 colMaxZero <- function(data) {
   return(sapply(c(data), function(a){
-    returnmmystrategy = c(3, 5, 8, 10, 13, 1, 26, 30, 2, 2)
-    ystrategy = c(3, 5, 8, 10, 13, 1, 26, 30, 2, 2)
-    (max(a, 0))
+    return(max(a, 0))
   }))
 }
-mystrategy = c(3, 5, 8, 10, 13, 1, 26, 30, 2, 2)
 
+#utility function for genetic algorithm
 colMinSoldiers <- function(data) {
   return(sapply(c(data), function(a){
     return(min(a, NUM_SOLDIERS))
   }))
 }
 
-genetic_find <- function(seed, iterations = 10, decay = 0.9, explore=.8, dropout=0.4){
+#genetic algorithm to explore the possibility space and try to find local optimas
+genetic_find <- function(seed, iterations = 100, decay = 0.95, explore=.8, dropout=0.4){
   current_best = seed
   current_best_result <- sum(sapply(seq(1, nrow(s), 1), function(i){
-    return(evaluate(current_best, s[i,]))
-  }
+      return(evaluate(current_best, s[i,]))
+    }
   ))
   current_explore = explore
   for(i in seq(from=1, to=iterations)){
     print(paste("Current best (", current_best_result,", iteration ", i,"): ", list(current_best), sep=""))
     perturbed <- colMinSoldiers(colMaxZero(current_best + (runif(length(CASTLE_VALUES), 0, 1) > dropout) * rnorm(n=length(CASTLE_VALUES), mean = 0, sd = current_explore) * (10 + current_best)))
+    
     #normalize
     perturbed <- round(NUM_SOLDIERS * perturbed / (sum(perturbed)), 0)
     while(sum(perturbed) != NUM_SOLDIERS) {
@@ -144,8 +121,8 @@ genetic_find <- function(seed, iterations = 10, decay = 0.9, explore=.8, dropout
     }
     print(paste("Evaluating perturbation ", list(perturbed), sep=""))
     perturbed_result <- sum(sapply(seq(1, nrow(s), 1), function(i){
-      return(evaluate(perturbed, s[i,]))
-    }
+        return(evaluate(perturbed, s[i,]))
+      }
     ))
     print(paste("Perturbed Result: ", perturbed_result))
     if(perturbed_result > current_best_result) {
@@ -157,5 +134,33 @@ genetic_find <- function(seed, iterations = 10, decay = 0.9, explore=.8, dropout
   return(current_best)
 }
 
-system.time(best <- genetic_find(c(6, 2, 2, 12, 16, 3, 2, 31, 4, 22), decay=0.995, explore=0.9, dropout=0.6, iterations=1000))
+
+#using various seed values, the resulting strategy is what the genetic algorithm ended up spitting out
+seed = c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+mystrategy = c(6, 2, 2, 12, 16, 3, 2, 31, 4, 22)
+#4, 5, 8, 10
+sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
+
+seed = c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+mystrategy = c(0, 3, 2, 12, 3, 22, 2, 31, 3, 22)
+#4, 6, 8, 10
+sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
+
+seed = c(1, 1, 11, 12, 1, 19, 24, 29, 1, 1)
+mystrategy = c(0, 1, 7, 8, 2, 22, 22, 31, 4, 3)
+#3, 4, 6, 7, 8
+sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
+
+seed = c(3, 5, 8, 10, 13, 1, 26, 30, 2, 2)
+mystrategy = c(0, 6, 8, 9, 12, 1, 27, 31, 2, 4)
+#2, 3, 4, 5, 7, 8
+sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
+
+seed = c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+mystrategy = c(0, 5, 7, 9, 16, 22, 2, 2, 33, 4)
+#2, 3, 4, 5, 6, 9
+sum(sapply(seq(1, nrow(s), 1), function(i){ return(evaluate(mystrategy, s[i,]))}))
+
+
+system.time(best <- genetic_find(seed, decay=0.995, explore=0.9, dropout=0.6, iterations=1000))
 
